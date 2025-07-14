8c5f5c3744da0e77a712f56a3a9a9a4a351c47da
Aleph <turdiev1@gmail.com>
2025-07-14 15:28:39 +0300

    fixed potential KeyError

--- bot.py
+++ bot.py
@@ -26,9 +26,24 @@ def init_bot():
         logging.critical("Token not found in environment. Edit '.env' file")
         exit(1)
     return telebot.TeleBot(token=str(TOKEN), parse_mode=None)
+
+def update_progress(ID, progress):
+    logging.info(f"Saving progress for chat {ID}")
+    with open(f"progress/{ID}", "w") as savefile:
+        savefile.write(progress)
+
+def load_progress(ID):
+    logging.info(f"Loading progress for chat {ID}")
+    try:
+        with open(f"progress/{ID}", "r") as savefile:
+            return savefile.read()
+    except FileNotFoundError:
+        logging.error(f"No savefile found for chat {ID}, returning \"start\"")
+        return "start"
     
 def snd_msg(progress, story, bot, ID):
     logging.info(f"Sending a message in chat {ID}")
+
     keyboard = types.ReplyKeyboardMarkup(
             resize_keyboard=True)
 
@@ -49,20 +64,6 @@ def snd_msg(progress, story, bot, ID):
         bot.send_message(ID, f"ERROR: {e}")
         logging.error(f"Encountered exception in snd_msg function: {e}")
 
-
-def update_progress(ID, progress):
-    logging.info(f"Saving progress for chat {ID}")
-    with open(f"progress/{ID}", "w") as savefile:
-        savefile.write(progress)
-
-def load_progress(ID):
-    logging.info(f"Loading progress for chat {ID}")
-    try:
-        with open(f"progress/{ID}", "r") as savefile:
-            return savefile.read()
-    except FileNotFoundError:
-        logging.error(f"No savefile found for chat {ID}, returning \"start\"")
-        return "start"
 
 def main():
     logging.basicConfig(level=logging.INFO, 
@@ -91,17 +92,26 @@ def main():
         
     @bot.message_handler(func=lambda message: True)
     def send_message(message):
+        ID = message.chat.id
         logging.info(f"""Received message \"{message.text}\" from chat {message.chat.id}""")
         current_progress = load_progress(message.chat.id)
+        
+        if current_progress not in story:
+            bot.send_message(ID, f"Story key not found: {current_progress}")
+            bot.send_message(ID, f"Falling back to 'start'")
+            update_progress(ID, "start")
+            current_progress = "start"
+            logging.warning(f"KeyError in {ID} chat; falling back to 'start'")
+
         for choice in story[current_progress]["choices"]:
             if message.text == choice["text"]:
                 current_progress = choice["next"]
-                snd_msg(current_progress, story, bot, message.chat.id)
-                update_progress(message.chat.id, current_progress)
+                snd_msg(current_progress, story, bot, ID)
+                update_progress(ID, current_progress)
                 break
         else:
-            bot.send_message(message.chat.id, "sry m8 not seeing this option")
-            logging.error(f"""Invalid option \"{message.text}\" in chat {message.chat.id}""")
+            bot.send_message(ID, "sry m8 not seeing this option")
+            logging.error(f"Invalid option \"{message.text}\" in chat {message.chat.id}")
             
 
     bot.polling()
